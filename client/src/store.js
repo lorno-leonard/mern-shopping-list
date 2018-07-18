7efecd246e8e692a8b1b58e7ee3018fbbe6dbfe7
Leonard Laput <online.lorno@gmail.com>
2018-07-18 11:57:11 +0800

    check if redux dev tools is available

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,16 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
+let _compose = null;
+if (typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined") {
+  _compose = compose(applyMiddleware(...middleware));
+} else {
+  _compose = compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+}
+
+const store = createStore(rootReducer, initialState, _compose);
 
 export default store;
